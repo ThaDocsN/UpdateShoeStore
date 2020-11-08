0d484918b00c26deeea15466a07640461f2ffb7f
Charles Johnson <35396909+ThaDocsN@users.noreply.github.com>
2020-11-07 22:05:11 -0600

    added greeting destination. Finished step 6

--- starter/app/src/main/java/com/udacity/shoestore/models/welcome/GreetingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/welcome/GreetingFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.GreetingFragmentBinding
 
 class GreetingFragment : Fragment() {
+
+    private lateinit var binding: GreetingFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.greeting_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.greeting_fragment, container, false)
+
+        binding.btnContinue.setOnClickListener {
+            findNavController().navigate(GreetingFragmentDirections.actionGreetingDestinationToInstructionsFragment())
+        }
+
+        return binding.root
     }
 }
