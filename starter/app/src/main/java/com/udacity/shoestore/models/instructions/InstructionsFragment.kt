92639d195a511f62070b11de01b19431833a43ad
Charles Johnson <35396909+ThaDocsN@users.noreply.github.com>
2020-11-07 22:22:55 -0600

    added instructions destination. Finished step 7

--- starter/app/src/main/java/com/udacity/shoestore/models/instructions/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/instructions/InstructionsFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionsFragmentBinding
 
 class InstructionsFragment : Fragment() {
+
+    private lateinit var binding: InstructionsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.instructions_fragment, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.instructions_fragment, container, false)
+
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(InstructionsFragmentDirections.actionInstructionsDestinationToShoeListFragment())
+        }
+
+        return binding.root
     }
+
 }
