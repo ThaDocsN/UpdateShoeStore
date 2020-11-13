423525d37851da4c8467c743921a01e66bc037bd
Charles Johnson <35396909+ThaDocsN@users.noreply.github.com>
2020-11-13 02:28:16 -0600

    removed clicklisteners from fragment to viewModel

--- starter/app/src/main/java/com/udacity/shoestore/models/login/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/models/login/LoginFragment.kt
@@ -1,11 +1,12 @@
 package com.udacity.shoestore.models.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.LoginFragmentBinding
@@ -21,15 +22,26 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
 
-        binding.btnLogin.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginDestinationToGreetingFragment())
-        }
+        binding.lifecycleOwner = this
 
-        binding.btnNewAccount.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginDestinationToGreetingFragment())
-        }
+        val viewModel: LoginViewModel by viewModels()
+
+        binding.viewModel = viewModel
+
+        viewModel.onLoginButton.observe(viewLifecycleOwner, { clicked ->
+            if (clicked) {
+                findNavController().navigate(LoginFragmentDirections.actionLoginDestinationToGreetingFragment())
+                viewModel.onLoginDone()
+            }
+        })
+
+        viewModel.onCreateButton.observe(viewLifecycleOwner, { clicked ->
+            if (clicked) {
+                findNavController().navigate(LoginFragmentDirections.actionLoginDestinationToGreetingFragment())
+                viewModel.onCreateDone()
+            }
+        })
 
         return binding.root
-
     }
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/models/login/LoginViewModel.kt
@@ -0,0 +1,35 @@
+package com.udacity.shoestore.models.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+    private val _onLoginButton = MutableLiveData<Boolean>()
+    val onLoginButton: LiveData<Boolean> = _onLoginButton
+
+    private val _onCreateButton = MutableLiveData<Boolean>()
+    val onCreateButton: LiveData<Boolean> = _onCreateButton
+
+    init {
+        _onLoginButton.value = false
+        _onCreateButton.value = false
+    }
+
+    fun onLoginClicked() {
+        _onLoginButton.value = true
+    }
+
+    fun onLoginDone() {
+        _onLoginButton.value = false
+    }
+
+    fun onCreateClicked() {
+        _onCreateButton.value = true
+    }
+
+    fun onCreateDone() {
+        _onCreateButton.value = false
+    }
+}
